cecd3f7b20725a5bf33879dc57d214493b1624fb
Simon Henes <simon.henes@live.de>
2023-11-30 15:51:46 +0100

    new_data

--- Wafermap_Project/App/model/model.py
+++ Wafermap_Project/App/model/model.py
@@ -11,19 +11,13 @@ import numpy as np
 
     # Data Visualization
 import matplotlib.pyplot as plt
-from matplotlib import gridspec
 
     # Machine/Deep Learning
 
 
 
 # Step 2: Read Data
-    # Data was seperated in a sample file (1% of orig data) to explore it without too long loading
-df = pd.read_pickle('./data/sample.pkl')
+    
 
 
 # Step 3: Prepare / Manipulate Data
-print(df.head())
-
-a = df['waferMap'][0]
-print(a, a.shape)
