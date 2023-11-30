7e17a17064cbce711e67b86f2f56372825419335
Simon Henes <simon.henes@live.de>
2023-11-30 14:17:31 +0100

    Update model.py

--- Wafermap_Project/App/model/model.py
+++ Wafermap_Project/App/model/model.py
@@ -23,22 +23,7 @@ df = pd.read_pickle('./data/sample.pkl')
 
 
 # Step 3: Prepare / Manipulate Data
-df = df.drop(['waferIndex'], axis = 1)
+print(df.head())
 
-def find_dim(x):
-    dim0=np.size(x,axis=0)
-    dim1=np.size(x,axis=1)
-    return dim0,dim1
-df['waferMapDim']=df.waferMap.apply(find_dim)
-
-df['failureNum']=df.failureType
-mapping_type={'Center':0,'Donut':1,'Edge-Loc':2,'Edge-Ring':3,'Loc':4,'Random':5,'Scratch':6,'Near-full':7,'none':8}
-
-df=df.replace({'failureNum':mapping_type})
-new_df = df[df['failureNum'] <= 7]
-new_df = new_df[new_df['waferMapDim'] == (32,32)]
-
-
-X = np.array([np.ravel(x) for x in new_df['waferMap']])
-print(X)
-print(X.shape)
+a = df['waferMap'][0]
+print(a, a.shape)
