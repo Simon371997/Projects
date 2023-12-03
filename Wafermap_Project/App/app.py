3dedf4092b120677d1bcf38a86dd212415516147
Simon Henes <simon.henes@live.de>
2023-12-03 15:42:20 +0100

    templates

--- Wafermap_Project/App/app.py
+++ Wafermap_Project/App/app.py
@@ -1,13 +1,33 @@
 # Imports
 from flask import Flask, render_template, url_for, request
 import numpy as np
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 
-
-
+# Initialize App
 app = Flask(__name__)
 
+# Load Model
+model = load_model('model.h5')
 
+# routes
+@app.route('/')
+def home():
+    return render_template('base.html')
+
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    if request == 'POST':
+        input_data = request.form['input_data']
+        prediction = make_prediction(input_data)
+        return render_template('predict.html')
+
+
+def make_prediction(input_data):
+    input_array = np.array([input_data])
+    
+    result = model.predict(input_array)
+    return result
 
 
 
