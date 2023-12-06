02789257a165d51a14800bda8fa1b026f6a12ddc
Simon Henes <simon.henes@live.de>
2023-12-06 20:08:22 +0100

    save model
    use model.state_dict anstatt model.save

--- Wafermap_Project/App/test.py
+++ Wafermap_Project/App/test.py
@@ -1,9 +1,10 @@
 from keras.models import load_model
 from keras.preprocessing import image
 import numpy as np
+import torch
 
 # Laden des gespeicherten Modells
-model = load_model('model.h5')
+model = torch.load('./model/models/torchCNN.pth')
 
 # Pfad zum Testbild
 image_path = '/Users/simon/Documents/Master/Sonstiges/Python/Projects/Wafermap_Project/App/model/data/ImageData/test/Center/center_49.jpg'
